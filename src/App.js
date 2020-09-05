53f1cbb8f2b6a8b178b0bb2caa05d97700e69e08
doc_mhvr <mahavir37chandaliya@gmail.com>
2020-09-05 22:18:48 +0530

    Added BrowserRouter and Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/home';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />    
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={HomePage} exact />    
+      </div>
+    </Router>
   );
 }
 
