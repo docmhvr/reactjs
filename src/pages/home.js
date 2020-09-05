3df3924d2b75b5265f4cdbc780b892cf9c614813
doc_mhvr <mahavir37chandaliya@gmail.com>
2020-09-05 22:11:17 +0530

    Initial commit

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => (
+		<>
+		<h1>Welcome to FOCUS!</h1>
+		<p>
+			We are an organisation aimed at increasing focus and mental capabilities.
+		</p>
+		</>
+	);
+
+export default HomePage;
